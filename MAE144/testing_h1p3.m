0c418a52795836b84cea1a259a2bd3c837d9c03e
alikattee99 <108766780+alikattee99@users.noreply.github.com>
2023-10-12 16:31:37 -0700

    ..

--- MAE144/testing_h1p3.m
+++ MAE144/testing_h1p3.m
@@ -1,9 +1,18 @@
 %% Test for P3
 clear all
 clc
-%syms p1 z1
 b=[ -1];
 a=[-10 0];
+h=0.01;
+omegabar=0;
+sp=true;
+T=tf(b,a);
+my_amswer=AK_C2D_match(b,a,h,omegabar,sp)
+matlab_answer=c2d(T,h,'matched')
+%%%% Test for P3-syms
+syms p1 z1
+b=[ z1];
+a=[p1 0];
 h=0.01;
 omegabar=0;
 sp=true;
